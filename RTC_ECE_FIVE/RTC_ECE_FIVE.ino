f17a4a066362403c8edf75bea40cc033e3bbdb0e
iTakedown27 <ezrabridger27@gmail.com>
2023-11-28 00:34:32 -0800

    code works better real time

--- RTC_ECE_FIVE/RTC_ECE_FIVE.ino
+++ RTC_ECE_FIVE/RTC_ECE_FIVE.ino
@@ -12,46 +12,6 @@ uRTCLib rtc(0x68); // Initialize RTC
 
 uint8_t alarmHour, alarmMin, alarmSec; // Time for the Alarm
 
-void setup() {
-  URTCLIB_WIRE.begin();
-  rtc.set(0, 57, 12, 7, 22, 11, 23);
-
-  lcd.begin(16, 2);
-}
-
-void loop() {
-  
-  if (digitalRead(modeButton) == HIGH) {
-    delay(3000);
-    uint8_t newMonth = 1, newDay = 1, newYear = 0, newHr = 0, newMin = 0, newSec = 0; // SetAlarm
-    int mode = 1; // 1 represents hour, 2 represents minute, 3 represents second
-
-    lcd.clear();
-    displayTime(newMonth, newDay, newYear, newHr, newMin, newSec);
-    
-    // if (digitalRead(upButton) == HIGH) {
-    //   switch (mode) {
-    //     case 1:
-    //       // displayTime(rtc.hour(), rtc.);
-    //     case 2:
-
-    //     case 3:
-
-    //     default:
-    //       mode = 1;
-    //   }
-    // } else if (digitalRead(downButton) == HIGH) {
-
-    // }
-
-    // // rtc.set(0, 57, 12, 7, 22, 11, 23);
-    // // lcd.clear();
-  } else {
-    displayTime(rtc.month(), rtc.day(), rtc.year(), rtc.hour(), rtc.minute(), rtc.second());
-    delay(1000);
-  }
-}
-
 void displayTime(uint8_t month, uint8_t day, uint8_t year, uint8_t hour, uint8_t minute, uint8_t second) {
   rtc.refresh();
 
@@ -98,6 +58,81 @@ void displayTime(uint8_t month, uint8_t day, uint8_t year, uint8_t hour, uint8_t
   lcd.print(second);
   
 }
+
+void setup() {
+  URTCLIB_WIRE.begin();
+  rtc.set(0, 57, 12, 7, 22, 11, 23);
+
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  
+  if (digitalRead(modeButton) == HIGH) {
+    // default values
+    uint8_t newMonth = 1, newDay = 1, newYear = 0, newHr = 0, newMin = 0, newSec = 0; 
+
+    lcd.clear(); // clears LCD
+    displayTime(newMonth, newDay, newYear, newHr, newMin, newSec);
+
+    // In the following for loop, mode 1 represents month, 2 represents day, 
+    // 3 represents year, 4 represents hour, 5 represents minute, 6 represents second
+    // When you set time, you have 5 seconds to do so per "mode"
+    for (int mode = 1; mode <= 6; mode++) {
+      delay(5000);
+      if (digitalRead(upButton) == HIGH) {
+        switch (mode) {
+          case 1:
+            displayTime(newMonth+1, newDay, newYear, newHr, newMin, newSec);
+            break;
+          case 2:
+            displayTime(newMonth, newDay+1, newYear, newHr, newMin, newSec);
+            break;
+          case 3:
+            displayTime(newMonth, newDay, newYear+1, newHr, newMin, newSec);
+            break;
+          case 4:
+            displayTime(newMonth, newDay, newYear, newHr+1, newMin, newSec);
+            break;
+          case 5:
+            displayTime(newMonth, newDay, newYear, newHr, newMin+1, newSec);
+            break;
+          case 6:
+            displayTime(newMonth, newDay, newYear, newHr, newMin, newSec+1);
+            break;
+        }
+      } else if (digitalRead(downButton) == HIGH) {
+        switch (mode) {
+        case 1:
+          displayTime(newMonth-1, newDay, newYear, newHr, newMin, newSec);
+          break;
+        case 2:
+          displayTime(newMonth, newDay-1, newYear, newHr, newMin, newSec);
+          break;
+        case 3:
+          displayTime(newMonth, newDay, newYear-1, newHr, newMin, newSec);
+          break;
+        case 4:
+          displayTime(newMonth, newDay, newYear, newHr-1, newMin, newSec);
+          break;
+        case 5:
+          displayTime(newMonth, newDay, newYear, newHr, newMin-1, newSec);
+          break;
+        case 6:
+          displayTime(newMonth, newDay, newYear, newHr, newMin, newSec-1);
+          break;
+      }
+    }
+    rtc.set(newSec, newMin, newHr, rtc.dayOfWeek(), newDay, newMonth, newYear);
+    displayTime(rtc.month(), rtc.day(), rtc.year(), rtc.hour(), rtc.minute(), rtc.second());
+  } 
+  }
+  else {
+    displayTime(rtc.month(), rtc.day(), rtc.year(), rtc.hour(), rtc.minute(), rtc.second());
+    delay(1000);
+  }
+}
+
 
 // void setTime() {
 //   // get input from user
