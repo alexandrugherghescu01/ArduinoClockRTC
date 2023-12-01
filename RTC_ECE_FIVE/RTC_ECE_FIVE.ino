a7cd24a0333dadef7dfb592220aa0af2bd9f8fe7
iTakedown27 <ezrabridger27@gmail.com>
2023-11-30 16:53:31 -0800

    Update RTC_ECE_FIVE.ino

--- RTC_ECE_FIVE/RTC_ECE_FIVE.ino
+++ RTC_ECE_FIVE/RTC_ECE_FIVE.ino
@@ -6,12 +6,17 @@
 
 const int RSPin = 1, EPin = 2, d4 = 4, d5 = 5, d6 = 6, d7 = 7; // LCD pins
 const int modeButton = 8, upButton = 9, downButton = 10; // button pins
+const int buzzerPin = 11; // buzzer pin
+const uint8_t alarm = URTCLIB_ALARM_1;
 
 LiquidCrystal lcd(RSPin, EPin, d4, d5, d6, d7); // Initialize LCD
 uRTCLib rtc(0x68); // Initialize RTC
 
+uint8_t date[] = {1, 1, 0, 0, 0, 0};
+
 uint8_t alarmHour, alarmMin, alarmSec; // Time for the Alarm
-int mode = 0; // modes: 0 (time), 1 (set time), 2 (set alarm)
+int mode = 0; // modes: 1 (set time), 2 (set alarm)
+// clicking button increments mode
 
 void setup() {
   URTCLIB_WIRE.begin();
@@ -68,71 +73,219 @@ void displayTime(uint8_t month, uint8_t day, uint8_t year, uint8_t hour, uint8_t
 }
 
 void loop() {
-  
+
+  if (rtc.alarmTriggered(alarm)) {
+    playAlarm();
+  }
+
   if (digitalRead(modeButton) == HIGH) {
-    // default values
-    uint8_t newMonth = 1, newDay = 1, newYear = 0, newHr = 0, newMin = 0, newSec = 0; 
+    mode++;
+    if (mode == 1) {
+      // default values
+      // date includes the new Month, day, year, hour, minute, and second, respectively
 
-    lcd.clear(); // clears LCD
-    displayTime(newMonth, newDay, newYear, newHr, newMin, newSec);
+      lcd.clear(); // clears LCD
+      displayTime(date[0], date[1], date[2], date[3], date[4], date[5]);
 
-    // In the following for loop, mode 1 represents month, 2 represents day, 
-    // 3 represents year, 4 represents hour, 5 represents minute, 6 represents second
-    // When you set time, you have 5 seconds to do so per "mode"
-
-    delay(2000);
-    // if (digitalRead(upButton) == HIGH) {
-  
-      
-    // } else if (digitalRead(downButton) == HIGH) {
-      
-    // }
-    
-    rtc.set(newSec, newMin, newHr, rtc.dayOfWeek(), newDay, newMonth, newYear);
-  } 
-  else {
+      // In the following for loop, mode 0 represents month, 1 represents day, 
+      // 2 represents year, 3 represents hour, 4 represents minute, 5 represents second
+      // When you set time, you have 5 seconds to do so per "mode"
+      setTime();
+    } else {
+      setAlarmTime();
+    }
+    mode = 0;
+  } else {
     displayTime(rtc.month(), rtc.day(), rtc.year(), rtc.hour(), rtc.minute(), rtc.second());
     delay(1000);
   }
 }
 
+void setTime() {
+  for (int i = 0; i < 6; i++) {
+    delay(5000);
+    if (digitalRead(upButton) == HIGH) {
+      date[i]++;
+      date[i] = checkTime(i, date[i]);
+      displayTime(date[0], date[1], date[2], date[3], date[4], date[5]); // display incremented time
+    } else if (digitalRead(downButton) == HIGH) {
+      date[i]--;
+      date[i] = checkTime(i, date[i]);
+      displayTime(date[0], date[1], date[2], date[3], date[4], date[5]); // display decremented time
+    }
+  }
+  rtc.set(date[0], date[1], date[2], rtc.dayOfWeek(), date[3], date[4], date[5]);
+  resetDate();
+}
 
-// void setTime() {
-//   // get input from user
-//   int tempTimeHrs = 0;
-//   int tempTimeMins = 0;
-  
-//   int clickUp = digitalRead(upPin);
-//   int clickDown = digitalRead(downPin);
-//   if (clickUp == HIGH) {
-//     tempTimeHrs++;    
-//   } 
+void resetDate() {
+  for (int i = 0; i < 6; i++) {
+    if (i == 1 || i == 2) {
+      date[i] = 1;
+    } else {
+      date[i] = 0;
+    }
+  }
+}
 
-//    if (clickDown == HIGH) {
-//     tempTimeHrs--;
+void setAlarmTime() {
+  for (int i = 3; i < 6; i++) {
+    delay(5000);
+    if (digitalRead(upButton) == HIGH) {
+      date[i]++;
+      date[i] = checkTime(i, date[i]);
+      displayTime(rtc.month(), rtc.day(), rtc.year(), date[3], date[4], date[5]);
+    } else if (digitalRead(downButton) == HIGH) {
+      date[i]--;
+      date[i] = checkTime(i, date[i]);
+    }
+    rtc.alarmSet(URTCLIB_ALARM_TYPE_1_NONE, date[5], date[4], date[3], rtc.dayOfWeek());
+    resetDate();
+  }
+}
+
+// check if time input is valid
+int checkTime(int entry, int tempTime) {
+  switch (entry) {
+    case 0:
+      if (tempTime >= 0) {
+        tempTime = tempTime % 12 + 1;
+      } else {
+        tempTime = 12 + tempTime;
+      }
+      break;
+    case 1:
+      if (tempTime >= 0) {
+        tempTime = tempTime % 31 + 1;
+      } else {
+        tempTime = 30 + tempTime;
+      }
+      break;
+    case 2:
+      if (tempTime >= 0) {
+        tempTime = tempTime % 99 + 1;
+      } else {
+        tempTime = 99 + tempTime;
+      }
+      break;
+    case 3:
+      if (tempTime >= 0) {
+        tempTime = tempTime % 12 + 1;
+      } else {
+        tempTime = 12 + tempTime;
+      }
+      break;
+    case 4:
+      if (tempTime >= 0) {
+        tempTime = tempTime % 60;
+      } else {
+        tempTime = 60 + tempTime;
+      }
+      break;
+    case 5:
+      if (tempTime >= 0) {
+        tempTime = tempTime % 60;
+      } else {
+        tempTime = 60 + tempTime;
+      }
+      break;
+  }
+  return tempTime;
+}
+
+void playAlarm() {
+    tone(buzzerPin,659.255);
+    delay(500);
+    tone(buzzerPin,587.33);
+    delay(500);
+    tone(buzzerPin,523.251);
+    delay(500);
+    tone(buzzerPin,587.33);
+    delay(400);
     
-//   }
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(10);
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(50);
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(400);
+    
+    
+    tone(buzzerPin,587.33);
+    delay(500);
+    noTone(buzzerPin);
+    delay(10);
+    tone(buzzerPin,587.33);
+    delay(500);
+    noTone(buzzerPin);
+    delay(50);
+    tone(buzzerPin,587.33);
+    delay(500);
+    noTone(buzzerPin);
+    delay(400);
 
-//   if (tempTimeHrs >= 0) {
-//       tempTimeHrs = tempTimeHrs % 12 + 1;
-//   } else {
-//       tempTimeHrs = 12 + tempTimeHrs;
-//   }
-//   // display on LCD
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(10);
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(50);
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(400);
 
-//   if (digitalRead(nextPin) == HIGH) {
-//     if (clickUp == HIGH) {
-//       tempTimeMins++;
-//     } else if (clickDown == HIGH) {
-//       tempTimeMins--;
-//     }
+    tone(buzzerPin,659.255);
+    delay(500);
+    tone(buzzerPin,587.33);
+    delay(500);
+    tone(buzzerPin,523.251);
+    delay(500);
+    tone(buzzerPin,587.33);
+    delay(400);
+  //  THIS PART WORKS
+    
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(10);
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(50);
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(400);
 
-//     if (tempTimeHrs >= 0) {
-//         tempTimeMins = tempTimeMins % 60;
-//     } else {
-//         tempTimeHrs = 60 + tempTimeHrs;
-//     }
-//     // display on LCD
-//   }
-// } 
+    tone(buzzerPin,587.33);
+    delay(500);
+    noTone(buzzerPin);
+    delay(10);
+    tone(buzzerPin,587.33);
+    delay(500);
+    noTone(buzzerPin);
+    delay(50);
+    
+    tone(buzzerPin,659.255);
+    delay(500);
+    noTone(buzzerPin);
+    delay(10);
+    tone(buzzerPin,587.33);
+    delay(500);
+    noTone(buzzerPin);
+    delay(10);
+    tone(buzzerPin,523.251);
+    delay(500);
+    noTone(buzzerPin);
+    delay(1000);
+}
 
